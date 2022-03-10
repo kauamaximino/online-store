8ae54b7a08bbd1df5e53446f53e1831c38a75471
FlavioVillar <flavio_villar@hotmail.com>
2022-03-10 17:16:02 -0300

    Requisito 01 (em grupo - TODOS) - Implemente o módulo de acesso à api do Mercado Livre

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,34 @@
 export async function getCategories() {
-  // Implemente aqui
+  try {
+    const url = 'https://api.mercadolibre.com/sites/MLB/categories'
+    const response = await fetch(url)
+    const data = await response.json();
+    return data
+  } catch (error) {
+    return error
+  }
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
+  const urlQuery = `https://api.mercadolibre.com/sites/MLB/search?q=${query}`
+  const urlCategory = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}`
+  const urlAll = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`
+  try {
+    if (categoryId && query) {
+      const response = await fetch(urlAll)   
+      const data = await response.json();
+      return data
+    } else if (query) {
+      const response = await fetch(urlQuery)
+      const data = await response.json();
+      return data
+    } else {
+      const response = await fetch(urlCategory)
+      const data = await response.json();
+      return data
+    }
+
+  } catch (error) {
+    return error
+  }
 }
