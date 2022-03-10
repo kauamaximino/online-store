27632f040ee19d894ec3a22a4d1fe72cca0cccd3
Kauã Maximino <kaua_maximino@outlook.com>
2022-03-10 18:44:51 -0300

    requisito 02 (grupo)

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Home from './components/Home';
+
+class App extends React.Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={ Home } />
+        </Switch>
+      </BrowserRouter>
+    );
+  }
+}
+
+export default App;
