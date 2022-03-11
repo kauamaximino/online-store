3a9731561a76d97d4753e1665b1ab777c62f4009
Guilherme Aranha <guilherme.aranha.ga@gmail.com>
2022-03-11 11:09:56 -0300

    requisito 01-03 Fix error lint

--- src/services/api.js
+++ src/services/api.js
@@ -1,34 +1,21 @@
 export async function getCategories() {
   try {
-    const url = 'https://api.mercadolibre.com/sites/MLB/categories'
-    const response = await fetch(url)
+    const url = 'https://api.mercadolibre.com/sites/MLB/categories';
+    const response = await fetch(url);
     const data = await response.json();
-    return data
+    return data;
   } catch (error) {
-    return error
+    return error;
   }
 }
 
 export async function getProductsFromCategoryAndQuery(categoryId, query) {
-  const urlQuery = `https://api.mercadolibre.com/sites/MLB/search?q=${query}`
-  const urlCategory = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}`
-  const urlAll = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`
+  const urlAll = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`;
   try {
-    if (categoryId && query) {
-      const response = await fetch(urlAll)   
-      const data = await response.json();
-      return data
-    } else if (query) {
-      const response = await fetch(urlQuery)
-      const data = await response.json();
-      return data
-    } else {
-      const response = await fetch(urlCategory)
-      const data = await response.json();
-      return data
-    }
-
+    const response = await fetch(urlAll);
+    const data = await response.json();
+    return data;
   } catch (error) {
-    return error
+    return error;
   }
 }
